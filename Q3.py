c47487ce5785140d94b5f696a8ab149b0c9e7dd7
MrKietho <kiettenbrinke@gmail.com>
2023-03-07 16:15:27 +0100

    Q3 pretty much finished

--- Q3.py
+++ Q3.py
@@ -1,30 +1,49 @@
 from pyspark import SparkConf, SparkContext
+from itertools import combinations
+import numpy as np
 
 """
-    1. Build covariance matrix
-    2. Use Var(X + Y + Z) = Var(X) + Var(Y) + Var(Z) + 2 * Cov(X, Y) + 2 * Cov(X, Z) + 2 * Cov(Y, Z)
-       to find the triplets of vectors with threshold aggregate variance
+    Solves Question 3 of the assignment by using the following steps:
+    1. Read the vectors from the file
+    1. Build covariance matrix of all pairs of vectors
+    3. Generate all triplets of vectors
+    4. Calculate the aggregate variance from of each triplet using Var(X + Y + Z) = Cov(X, X) + Cov(Y, Y) + Cov(Z, Z) + 2 * Cov(X, Y) + 2 * Cov(X, Z) + 2 * Cov(Y, Z)
+    5. Filter triplets with aggregate variance less than or equal to threshold
 """
 
-sc = SparkContext(conf=SparkConf().setAppName("Q3").setMaster("local[*]").set("spark.executor.memory", "8g").set("spark.driver.memory", "8g").set("spark.executor.heartbeatInterval", "600").set("spark.driver.cores", "4").set("spark.executor.cores", "4"))
+# Methods to calculate covariance and aggregate variance
+mean = lambda x: sum([i for i in x]) / len(x)
+covariance = lambda x, y: sum( [ (x[i] - mean(x)) * (y[i] - mean(y)) for i in range(len(x)) ] ) / (len(x) - 1)
+agg_variance = lambda x, y, z: covariances[(x, x)] + covariances[(y, y)] + covariances[(z, z)] + 2 * covariances[(x, y)] + 2 * covariances[(x, z)] + 2 * covariances[(y, z)]
+
+sc = SparkContext(conf=SparkConf().setAppName("Q3").setMaster("local[*]"))
 
 threshold = 410
 
+# Load vector file
 path = "vectors.csv"
 vector_file = sc.textFile(path)
+
+# Create a dictionary of vectors (ID, Vector)
 vectors = vector_file.map(lambda x: x.split(",")).map(lambda x: (x[0], [int(i) for i in x[1].split(";")])).collectAsMap()
 
-pairs = vector_file.map(lambda x: x.split(",")[0])
-#TODO: Find a faster way to generate pairs, this is unnecessarily slow probably because of sting comparisons
-pairs = pairs.cartesian(pairs).filter(lambda x: x[0] <= x[1]).map(lambda x: ((x[0], x[1]), (vectors[x[0]], vectors[x[1]])))
+vector_ids = list(vectors.keys())
 
-mean = lambda x: sum([i for i in x]) / len(x)
-covariance = lambda x, y: sum( [ (x[i] - mean(x)) * (y[i] - mean(y)) for i in range(len(x)) ] ) / (len(x) - 1)
+# Create a dictionary of vector id pairs
+vector_pairs = list(combinations(vector_ids, 2)) + [(x, x) for x in vector_ids]
 
-covariances = pairs.mapValues(lambda x: covariance(x[0], x[1])).take(10)
+# Calculate covariance matrix for all pairs of vectors
+covariance_values = np.cov([vectors[x] for x in vector_ids])
 
-#TODO find triplets with threshold aggregate variance using Var(X + Y + Z) = Var(X) + Var(Y) + Var(Z) + 2 * Cov(X, Y) + 2 * Cov(X, Z) + 2 * Cov(Y, Z)
+# Create a dictionary of covariances
+covariances = {(vector_ids[i], vector_ids[j]): covariance_values[i][j] for i in range(len(vector_ids)) for j in range(len(vector_ids))}
 
-print(covariances)
+# Create a list of all triplets of vectors
+vector_triples = list(combinations(vector_ids, 3))
 
+# Calculate aggregate variance for each triplet and filter triplets with aggregate variance less than or equal to threshold
+result = sc.parallelize(vector_triples).map(lambda x: ((x[0], x[1], x[2]), agg_variance(x[0], x[1], x[2]))).filter(lambda x: x[1] <= threshold).collect()
 
+print(result)
+
+sc.stop()
